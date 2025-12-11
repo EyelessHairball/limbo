013d55fd4c6c4b662760645793239a7143168bb0
L3FT_4.D34D <79947384+EyelessHairball@users.noreply.github.com>
2025-12-11 06:53:43 -0700

    Update script.js

--- script.js
+++ script.js
@@ -243,6 +243,7 @@ async function transitionToRotary() {
   });
   
   await Promise.all(spinPromises);
+  await sleep(200);
   
   const startPositions = Array.from(keys).map(key => {
     const rect = key.getBoundingClientRect();
